e1b7a554b1dabc2c226ca67acddcb2ac1b4b926b
JH-BIT <53570448+JH-BIT@users.noreply.github.com>
2022-04-09 17:09:01 +0800

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,13 @@
+$input = 'F:\TEST\out' #杩欓噷鏀逛负浣犵殑婧愬浘鐗囪矾寰?
+
+$output = 'E:\test' #杩欓噷鏀逛负浣犵殑鏂板浘鐗囪矾寰?
+
+explorer $input
+
+echo 暂停请点击文字，继续请按回车键，中止点右上角关闭
+
+Read-Host '请复制图片到' $input '下，然后按下回车键'
+
+.\realesrgan-ncnn-vulkan.exe -i $input -o $output -n realesrgan-x4plus-anime
+
+explorer $output 
